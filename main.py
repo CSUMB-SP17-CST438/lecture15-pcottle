5c353d17c2bc80a5791dba945ef49c7803bb49a6
mjohnson880 <morganrjohnson@gmail.com>
2017-03-15 01:27:58 +0000

    working on it.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-
 # Alright lets make a pizza!
 
 from __future__ import print_function
+import random
 
 def getCheese():
     return 'A bit of Mozzarella cheese'
@@ -9,9 +9,13 @@ def getCheese():
 def getSauce():
     return 'some basic Marinara sauce'
 
+def getPepperoni():
+    return '%d pepperoni slices' % random.randrange(3, 10)
+    
 def getIngredients():
     return [
         getCheese(),
+        getPepperoni(),
         getSauce()
     ]
 
@@ -21,6 +25,7 @@ def printPizza():
     print("\n".join(
         [str(num + 1) + ': ' + i for (num, i) in enumerate(ingredients)]
     ))
+
 
 print('''
  ____  __  ____  ____   __     ____  __  _  _  ____
